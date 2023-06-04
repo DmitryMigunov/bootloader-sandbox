dfc0454f0d1a7a5ca996252446cdaee302939e36
Dmitry Migunov <migunov.dmitrij@gmail.com>
2023-06-04 19:58:21 +0300

    Remove cursor + fix player position

--- sandbox.asm
+++ sandbox.asm
@@ -8,6 +8,11 @@ int 10h
 mov ax, 0B800h
 mov es, ax ; ES:DI <- B800:0000
 
+; hide blinking cursor
+mov ch, 0xff
+mov ah, 0x1
+int 10h
+
 jmp draw
 loop:
 ; input
@@ -39,15 +44,15 @@ down_pressed:
 	jmp draw
 left_pressed:
 	dec word [player_x]
-	cmp word [player_x], 2
+	cmp word [player_x], 1
 	jg draw
-	mov word [player_x], 2
+	mov word [player_x], 1
 	jmp draw
 right_pressed:
 	inc word [player_x]
-	cmp word [player_x], 78*2
+	cmp word [player_x], 78
 	jl draw
-	mov word [player_x], 78*2
+	mov word [player_x], 78
 	jmp draw
 
 draw:
@@ -95,9 +100,11 @@ sub di, 79*2
 mov [es:di], ax
 
 ; draw player
-imul di, [player_y], 160
+imul di, [player_y], 80
 add di, [player_x]
-mov word [es:di], 2020h
+imul di, 2
+; c | (((backcolour << 4) | (forecolour & 0x0F)) << 8)
+mov word [es:di], ((0xf * 0xb) + 0xa + 0x3)| (((0xa << 4) | (0xc & 0x0f)) << 8)
 
 next:
 ; delay
@@ -109,8 +116,8 @@ cmp [046ch], bx
 jl .delay
 jmp loop
 
-player_y: dw 10
-player_x: dw 10
+player_y: dw 1
+player_x: dw 1
 
 times 510-($-$$) db 0
 dw 0aa55h
